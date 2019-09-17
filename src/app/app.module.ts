81354819a615b62681ecc081735f8a5627bce92d
roman <roman.predein@apiumtech.com>
2019-09-18 00:04:34 +0200

    dependencies fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { LoginComponent } from './components/login/login.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { ButtonComponent } from './components/button/button.component';
 import { BtnpageComponent } from './components/btnpage/btnpage.component';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
